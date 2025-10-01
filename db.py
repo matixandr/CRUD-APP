8b1c0395a906028952360503671851e2cb0c604b
MatixAndr09 <matixandr09@gmail.com>
2025-10-01 19:48:36 +0200

    Changed in db.py to modern approach for columns (mapped_column)

--- db.py
+++ db.py
@@ -1,21 +1,25 @@
-from sqlalchemy import Column, INTEGER, VARCHAR, TIMESTAMP, DATE, ForeignKey, Engine
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base, relationship, mapped_column, Mapped
+from sqlalchemy import INTEGER, VARCHAR, TIMESTAMP, DATE, ForeignKey, Engine
+from sqlalchemy.orm.relationships import _RelationshipDeclared
 from sqlalchemy.dialects.postgresql import ENUM
+from datetime import datetime, date
+from typing import Any
 import sqlalchemy
 import dotenv
 import os
+
 
 Base = declarative_base()
 
 class Tasks(Base):
     __tablename__ = "tasks"
-    task_id = Column(INTEGER, primary_key=True, autoincrement=True)
-    users = relationship("Users", back_populates="tasks")
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False)
-    user_id = Column(INTEGER, ForeignKey('users.id'))
-    task_name = Column(VARCHAR(128))
-    due_date = Column(DATE)
-    status = Column(
+    users: _RelationshipDeclared[Any] = relationship("Users", back_populates="tasks")
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
+    task_id: Mapped[int] = mapped_column(INTEGER, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(INTEGER, ForeignKey('users.id'))
+    task_name: Mapped[str] = mapped_column(VARCHAR(128))
+    due_date: Mapped[date] = mapped_column(DATE)
+    status = mapped_column(
         ENUM(
             'pending',
             'in-progress',
@@ -25,7 +29,7 @@ class Tasks(Base):
         nullable=False,
         default='pending'
     )
-    priority = Column(
+    priority = mapped_column(
         ENUM(
             'low',
             'medium',
@@ -38,11 +42,11 @@ class Tasks(Base):
 
 class Users(Base):
     __tablename__ = "users"
-    tasks = relationship("Tasks", back_populates="users")
-    id = Column(INTEGER, primary_key=True)
-    created_at = Column(TIMESTAMP)
-    username = Column(VARCHAR(64))
-    role = Column(VARCHAR(32))
+    tasks: _RelationshipDeclared[Any] = relationship("Tasks", back_populates="users")
+    id: Mapped[int] = mapped_column(INTEGER, primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP)
+    username: Mapped[str] = mapped_column(VARCHAR(64))
+    role: Mapped[str] = mapped_column(VARCHAR(32))
 
 def db_init() -> Engine:
     dotenv.load_dotenv()
